1feb49acfa4967b53b81bd0931b510ea0a15b37e
Gabriel Martinez <gmartinez@itexico.net>
2016-08-25 12:20:04 -0500

    add mongo client

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 * so all the required libraries are downloaded and installed.
 **/
 
+const MongoClient = require('mongodb').MongoClient
+
 var express = require('express');
 // Create a new express.js  wen app
 var app = express();
@@ -19,5 +21,12 @@ require('./routes')(app);
 
 //This file has been called directly with
 // node index.js. Start the server
-app.listen(8081);
-console.log('ur mother fucker app is running on http://localhost:8081');
+MongoClient.connect('mongodb://funtexico:funtexic0@ds031741.mlab.com:31741/memes', (err, database) => {
+	if(err) return console.log(err);
+	// Global variable to be used outside
+	db = database;
+	console.log('Connected to: ' + db.databaseName + ' from mlabs.com');
+	app.listen(8081, () => {
+		console.log('The app is running on http://localhost:8081');
+	});
+});
